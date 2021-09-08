d75037636a0d53502e829fc191fe353fe3e454ae
Victor Stepin <vicstep1994@gmail.com>
2021-09-08 21:27:35 +0700

    Initial commit

---
+++ HuntTheWumpus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuntTheWumpus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
